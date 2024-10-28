d5113a726520a9b0ba55b7748a10d9d2e5e797c3
GMan <grahamisaac8@gmail.com>
2024-10-28 14:14:15 -0600

    removed devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import {thunk} from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { quote, scroll } from './reducers'
 
@@ -12,6 +11,6 @@ const initialState = {}
 
 const middleware = [thunk]
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(reducer, initialState(applyMiddleware(...middleware)))
 
 export default store
